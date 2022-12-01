6c46d828d50d1bc65f5915f0c69a4d2ad90376f3
AnochaPay <Anocha_ping@hotmail.com>
2022-12-01 10:08:03 +0700

    first commit

---
+++ Version Updata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace camera_show {
+    class Version_Updata {
+        //V1.02
+        //Edit code to read easy
+        //Edit file config from txt to csv
+        //Edit function scan min max value camera to scan fast
+        //Edit SmallChange set camera
+        //Edit bug scan min max value camera
+        //Add check digit sn
+        //Add adjust degree
+        //Add check led add step
+
+        //V1.03
+        //Add step fail in result.txt example "(3)OverTime\r\nFAIL"
+
+        //V2022.04
+        //Add path head 1 2 3 4
+
+        //V2022.05
+        //Add blinkLed mode
+
+        //V2022.06
+        //Add ReAdjust Camera
+
+        //V2022.07
+        //Edit set gamma to address camera
+    }
+}
